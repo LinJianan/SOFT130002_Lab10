8d1941c7f7d173678157b1d44c162e34e47ac31b
Lin Jia'nan <jnlin16@fudan.edu.cn>
2020-06-02 01:01:30 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'alpacagod');
+define('DBPASS', 'lol5555555');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
